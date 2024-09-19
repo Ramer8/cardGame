47b9ced47c278825b25a547c674261d73f689cdd
Ramer8 <75700901+Ramer8@users.noreply.github.com>
2024-09-20 00:57:18 +0200

    Update: change background color

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import "./App.css"
+import AudioRecorder from "./components/AudioRecorder/AudioRecorder"
 import CardGroup from "./components/CardGroup/CardGroup"
+import SpeechApp from "./components/SpeechApp/SpeechApp"
+import SpeechToNumber from "./components/SpeechToNumbers/SpeechToNumbers"
+import SpeechToText from "./components/SpeechToText/SpeechToText"
+import Grid from "./components/Grid/Grid"
 
 function App() {
   return (
@@ -7,6 +12,12 @@ function App() {
       <h1>Card Game</h1>
       <h2>Choose a Card</h2>
       <CardGroup />
+      <Grid />
+      {/* <SpeechApp /> */}
+
+      {/* <SpeechToText />
+      <SpeechToNumber />
+      <AudioRecorder /> */}
     </>
   )
 }
