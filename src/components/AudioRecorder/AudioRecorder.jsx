483c1cac4c0fdb5a25beef80b1bdecc6440ce9a1
Ramer8 <75700901+Ramer8@users.noreply.github.com>
2024-09-20 00:56:20 +0200

    Feat: AudioRecorder component

---
+++ src/components/AudioRecorder/AudioRecorder.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react"
+
+const AudioRecorder = () => {
+  const [isRecording, setIsRecording] = useState(false)
+  const [stream, setStream] = useState(null)
+
+  // Solicitar permiso para el micrófono
+  const enableMic = async () => {
+    try {
+      const mediaStream = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+      })
+      setStream(mediaStream)
+      setIsRecording(true)
+    } catch (error) {
+      console.error("Error al habilitar el micrófono: ", error)
+    }
+  }
+
+  // Detener el uso del micrófono
+  const disableMic = () => {
+    if (stream) {
+      stream.getTracks().forEach((track) => track.stop()) // Detener el flujo de audio
+      setIsRecording(false)
+    }
+  }
+
+  return (
+    <div>
+      <h2>Grabar audio</h2>
+      {isRecording ? (
+        <button onClick={disableMic} style={{ background: "blue" }}>
+          Detener grabación
+        </button>
+      ) : (
+        <button onClick={enableMic} style={{ background: "red" }}>
+          Iniciar grabación
+        </button>
+      )}
+    </div>
+  )
+}
+
+export default AudioRecorder
